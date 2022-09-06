15b8ff9fd5cf2d6971ddf6d2d438fdaec9c7553c
KyleTryon <Kyle.Tryon@circleci.com>
2022-09-06 13:57:30 -0400

    feat: migrate to orb tools 11
    chore: shellcheck

--- src/scripts/create-review.sh
+++ src/scripts/create-review.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 function get_artifacts {
   artifact_request=$(curl --request GET \
       --url "https://circleci.com/api/v2/project/gh/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM}/artifacts" \
@@ -5,9 +7,9 @@ function get_artifacts {
 
   for row in $(echo "${artifact_request}" | jq -c '.items[]'); do
     _jq() {
-     echo ${row} | jq -r ${1}
+     echo "${row}" | jq -r "${1}"
     }
-    if [[ $(_jq '.path') == $ARTIFACT_PATTERN ]]; then
+    if [[ $(_jq '.path') == "$ARTIFACT_PATTERN" ]]; then
       ARTIFACT_LOCATION=$(_jq '.url')
       echo "Using ${ARTIFACT_LOCATION} as artifact."
       return 0
@@ -31,7 +33,7 @@ function create {
     -H "Accept: application/vnd.heroku+json; version=3" \
     -H "Authorization: Bearer ${HEROKU_API_KEY}")
 
-  echo $heroku_request | jq -C
+  echo "$heroku_request" | jq -C
 }
 
 get_artifacts
