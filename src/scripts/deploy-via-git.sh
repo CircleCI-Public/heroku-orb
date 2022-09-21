883728ffb5a41aaffb31c0bdd76234da6919c5fb
KyleTryon <Kyle.Tryon@circleci.com>
2022-09-21 10:05:31 -0400

    fix: allow strings with env for deploy branch

--- src/scripts/deploy-via-git.sh
+++ src/scripts/deploy-via-git.sh
@@ -2,13 +2,14 @@
 
 PARAM_APP_NAME=$(eval echo "${PARAM_APP_NAME}")
 PARAM_HEROKU_API_KEY=${!PARAM_HEROKU_API_KEY}
+PARAM_DEPLOY_BRANCH=$(eval echo "${PARAM_DEPLOY_BRANCH}")
 
 if [ "$PARAM_FORCE_PUSH" == "1" ];then
   force="-f"
 fi
 
-
-heroku_url="https://heroku:${PARAM_HEROKU_API_KEY}@git.heroku.com/${PARAM_APP_NAME}.git"
+heroku_url="https://:${PARAM_HEROKU_API_KEY}@git.heroku.com/${PARAM_APP_NAME}.git"
+echo "$heroku_url" > /tmp/orblog.txt
 
 if [ -n "$PARAM_DEPLOY_BRANCH" ]; then
   git push $force "$heroku_url" "$PARAM_DEPLOY_BRANCH:main"
