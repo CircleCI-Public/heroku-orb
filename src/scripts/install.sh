230d992f14aac6772a11ea83ee22d6889be4d84c
KyleTryon <Kyle.Tryon@circleci.com>
2022-09-23 13:27:07 -0400

    refactor: improve if statement

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $(command -v heroku) == "" ]]; then
+if ! command -v heroku &> /dev/null; then
   curl https://cli-assets.heroku.com/install.sh | sh
 else
   echo "Heroku is already installed. No operation was performed."
