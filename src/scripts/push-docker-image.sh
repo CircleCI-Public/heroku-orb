be9d6839dc4db2a99274ad4c237009c51bc4abdf
Emil Janitzek <emil@pixel2.se>
2023-08-24 13:50:26 +0200

    feat: add support for context path on push docker image command

--- src/scripts/push-docker-image.sh
+++ src/scripts/push-docker-image.sh
@@ -6,6 +6,10 @@ if [ "$PARAM_RECURSIVE" == "1" ];then
   set -- "$@" --resursive
 fi
 
+if [ -n $PARAM_CONTEXT_PATH ];then
+  set -- "$@" "--context-path $PARAM_CONTEXT_PATH"
+fi
+
 set -x
 heroku container:push -a "$PARAM_APP_NAME" "$@" "$PARAM_PROCESS_TYPES"
 set +x
